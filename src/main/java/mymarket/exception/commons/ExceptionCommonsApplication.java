9ef57bd8831868737a38d907e5bc1b155213253b
enzo <enzo1999@live.com.ar>
2021-10-17 20:30:33 -0300

    first commit

---
+++ src/main/java/mymarket/exception/commons/ExceptionCommonsApplication.java
@@ -0,0 +1,6 @@
+package mymarket.exception.commons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionCommonsApplication {}
---
+++ src/test/java/mymarket/exception/commons/ExceptionCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package mymarket.exception.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
